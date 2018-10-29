57885e1a22a8b2fa2cf843c8654ca6857917d8e9
Enikő Tóthová <toeraab.avf@gmail.com>
2018-10-29 12:55:56 +0100

    content of day1 added

---
+++ week-02/day-01/src/hu/greenfox/bleaksmile/Bmi.java
@@ -0,0 +1,15 @@
+package hu.greenfox.bleaksmile;
+
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 49.7;
+        double heightInM = 1.59;
+        double bmi = massInKg/(heightInM*heightInM);
+
+        System.out.println(bmi);
+
+
+        // Print the Body mass index (BMI) based on these values
+
+    }
+}
---
+++ week-02/day-01/src/hu/greenfox/bleaksmile/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the terminal window.
+        System.out.println("Hello, Enikő!");
+        int birthYear = 1990;
+        System.out.println(birthYear);
+        String firstName = "Eniko";
+        String lastName = "Tothova";
+        System.out.println(firstName + " " + lastName);
+        String  fullName = (firstName + " " + lastName);
+        System.out.println(fullName);
+
+
+    }
+
+
+}
