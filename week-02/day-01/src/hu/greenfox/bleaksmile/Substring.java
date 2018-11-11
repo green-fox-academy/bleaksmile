dc471cbe0e44f998954d413976dc0a32a2b46b7f
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-11 10:59:37 +0100

    substring exercises solved with loop

--- week-02/day-01/src/hu/greenfox/bleaksmile/Substring.java
+++ week-02/day-01/src/hu/greenfox/bleaksmile/Substring.java
@@ -5,15 +5,16 @@ public class Substring {
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
 
-//  Example:
+    //  Example:
 //System.out.println(subStr("this is what I'm searching in", "searching"));
 //  should print: `17`
 //System.out.println(subStr("this is what I'm searching in", "not"));
 //  should print: `-1
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            System.out.println(contains("Imtsoearchin", "tea"));
-        }
+        System.out.println(subStr("imtkltearchin", "tea"));
+        // System.out.println(substring("this is what I'm searching in", "not"));
+    }
 
         /*public static int substring(String input1, String input2) {
 
@@ -24,31 +25,25 @@ public class Substring {
             return -1;
         }*/
 
-      //  public static int substring(String input1, String input2){
-
-
-        //}
-        public static int contains (String a, String b){
-            int j=0;
-            int k =0;
-            int index;
-
-                for (int i = 0; i < a.length(); i++) {
-                   if (j < b.length()) {
-                       char c = b.charAt(j);
-                       char d = a.charAt(i);
-                       if (c == d) {
-                           j++;
-                           k = i;
-                       }
-                   }
+    public static int subStr(String input, String part) {
+        int contains = 0;
+        for (int i = 0; i < input.length() - part.length(); i++) {
+            int index = 0;
+            if (part.charAt(index) == input.charAt(i)) {
+                for (int j = 0; j < part.length(); j++) {
+                    if (part.charAt(j) == input.charAt(j + i)) {
+                        contains++;
+                    }
+                    if (contains == part.length()) {
+                        return i;
+                    }
                 }
-
-                if(j==0){
-                    return -1;
-                }
-
-            k = k- b.length()+1;
-            return k;
+            }
         }
+        return -1;
+    }
+
+
+
+
 }
