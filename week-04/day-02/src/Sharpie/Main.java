494040b85e17bd6293686e5747cff4cb21cdb730
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-13 11:55:03 +0100

    creates Sharpie class with field values and use method

---
+++ week-04/day-02/src/Sharpie/Main.java
@@ -0,0 +1,29 @@
+package Sharpie;
+
+public class Main {
+
+    /*
+
+    Create Sharpie class
+    We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+    When creating one, we need to specify the color and the width
+    Every sharpie is created with a default 100 as inkAmount
+    We can use() the sharpie objects
+    which decreases inkAmount
+     */
+    public static void main(String[] args) {
+        Sharpie mine = new Sharpie("blue", 3.6f);
+        Sharpie yours = new Sharpie("green", 5.6f);
+
+        System.out.println(mine.getInkAmount());
+        System.out.println(yours.use());
+        System.out.println(yours.use());
+        System.out.println(yours.getInkAmount());
+
+        System.out.println(mine.getInkAmount());
+
+
+
+
+    }
+}
