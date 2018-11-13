aec46bc025ee16ed260b90d3ac6b1836c6d5d6ea
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-13 11:17:36 +0100

    creating different examples of the PostIt class

---
+++ week-04/day-02/src/PostIt/Main.java
@@ -0,0 +1,19 @@
+package PostIt;
+  /* Create a PostIt class that has
+            a backgroundColor
+            a text on it
+            a textColor
+            Create a few example post-it objects:
+    an orange with blue text: "Idea 1"
+    a pink with black text: "Awesome"
+    a yellow with green text: "Superb!"
+    */
+
+public class Main {
+    public static void main(String[] args) {
+        PostIt postOne = new PostIt("orange","Idea 1", "blue");
+        PostIt postTwo = new PostIt("pink", "Aweseome", "black");
+        PostIt postThree = new PostIt("yellow", "Superb", "green");
+
+    }
+}
