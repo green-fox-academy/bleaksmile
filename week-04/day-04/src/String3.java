69f284c1f6bb3f49f94746aecff0822d33e0ce6c
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-15 13:31:44 +0100

    handles null string

--- week-04/day-04/src/String3.java
+++ week-04/day-04/src/String3.java
@@ -2,10 +2,13 @@ public class String3 {
     // Given a string, compute recursively a new string where all the
     // adjacent chars are now separated by a "*".
     public static void main(String[] args) {
-        System.out.println(textWithAsterisk("example"));
+        System.out.println(textWithAsterisk("apple"));
     }
 
     public static String textWithAsterisk(String text) {
+        if (text.length() == 0) {
+            return "";
+        }
         String output = Character.toString(text.charAt(0));
         if (text.length() == 1) {
             return output;
