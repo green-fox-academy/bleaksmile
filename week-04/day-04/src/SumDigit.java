c797ba2b13414b4b89301111ac1eb39e74782c8b
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-15 11:27:18 +0100

    adds the digits of the number together

---
+++ week-04/day-04/src/SumDigit.java
@@ -0,0 +1,16 @@
+public class SumDigit {
+    // Given a non-negative int n, return the sum of its digits recursively (no loops).
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    public static void main(String[] args) {
+        System.out.println(sumDigit(125));
+    }
+
+    public static int sumDigit(int n) {
+        if (n < 10) {
+            return n;
+        } else {
+            return n % 10 + sumDigit(n / 10);
+        }
+    }
+}
