0c3e3a5c4d93facbd3bd616ffc8d1de7ca48d616
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-15 10:55:28 +0100

    raises to a power the given number

---
+++ week-04/day-04/src/Power.java
@@ -0,0 +1,19 @@
+public class Power {
+    // Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+    public static void main(String[] args) {
+
+        System.out.println(powerN(5,3));
+
+    }
+    public static int powerN(int num, int power){
+        if (power == 0){
+            return 1;
+        }else if(power == 1){
+            return num;
+        } else {
+            return num * powerN(num, power -1);
+        }
+
+    }
+}
