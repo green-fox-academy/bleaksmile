4a1fa8f410455d58ded732c860d626d2711ace1f
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-15 14:14:00 +0100

    returs factorial value of the n number

---
+++ week-04/day-04/src/Refactorio.java
@@ -0,0 +1,16 @@
+public class Refactorio {
+    // Create a recursive function called `refactorio`
+    // that returns it's input's factorial
+
+    public static void main(String[] args) {
+        System.out.println(refactorio(3));
+    }
+
+    public static int refactorio(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * refactorio(n - 1);
+        }
+    }
+}
