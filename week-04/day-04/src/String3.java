f86135ec5886bdf60a4fb1aa8b1cbb23c97b5691
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-15 13:21:47 +0100

    adds asterix into the string

--- week-04/day-04/src/String3.java
+++ week-04/day-04/src/String3.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class String3 {
+    // Given a string, compute recursively a new string where all the
+    // adjacent chars are now separated by a "*".
+    public static void main(String[] args) {
+        System.out.println(textWithAsterisk("apple"));
+
+    }
+
+    public static String textWithAsterisk(String text) {
+        String output = Character.toString(text.charAt(0));
+        if (text.length() == 1) {
+            return output;
+        } else {
+            return output + '*' + textWithAsterisk(text.substring(1));
+        }
+    }
 }
+
