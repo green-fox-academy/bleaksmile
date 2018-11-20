cf86f8c5e1c0683be59ecd102ada9a0f617cc4b2
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-20 11:40:23 +0100

    creates the GFA organization with several classes

---
+++ week-05/day-02/src/Exercise/Cohort.java
@@ -0,0 +1,28 @@
+package Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cohort {
+    private String name;
+    private List<Student> students;
+    private List<Mentor> mentors;
+
+    public Cohort(String name) {
+        this.name = name;
+        students = new ArrayList<>();
+        mentors = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void addMentor(Mentor mentor) {
+        mentors.add(mentor);
+    }
+
+    public void info(){
+        System.out.println("The " + this.name + " cohort has " + students.size() + " students and " + mentors.size() + " mentors.");
+    }
+}
---
+++ week-05/day-02/src/Exercise/Sponsor.java
@@ -0,0 +1,31 @@
+package Exercise;
+
+public class Sponsor extends Person {
+    String company;
+    int hiredStudents;
+
+    public Sponsor(String name, int age, String gender, String company) {
+        super(name, age, gender);
+        this.company = company;
+        this.hiredStudents = 0;
+    }
+
+    public Sponsor() {
+        super();
+        this.company = "Google";
+        this.hiredStudents = 0;
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + getName() + ", a " + getAge() + " year old " + getGender() + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+    }
+
+    public void hire() {
+        hiredStudents++;
+    }
+
+    public void getGoal() {
+        System.out.println("My goal is: Hire brilliant junior software developers.");
+    }
+
+}
