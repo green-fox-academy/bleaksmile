4c1ecb7556b9714499b1b297e0bf70c2c9ea37bf
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-21 14:12:48 +0100

    uses compareTo method for different classes

---
+++ week-05/day-03/src/Comparable/FleetOfThings.java
@@ -0,0 +1,38 @@
+package Comparable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+
+        Thing milk = new Thing("Get milk");
+        Thing obstacle = new Thing("Remove the obstacles");
+        Thing stand = new Thing("Stand up");
+        stand.complete();
+        Thing eat = new Thing("Eat lunch");
+        Thing listen = new Thing ("Listen to music");
+        listen.complete();
+        //eat.complete();
+        fleet.add(milk);
+        fleet.add(obstacle);
+        fleet.add(stand);
+        fleet.add(eat);
+        fleet.add(listen);
+        System.out.println(fleet);
+        System.out.println();
+
+        Collections.sort(fleet.getThings());
+        System.out.println(fleet);
+
+
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+    }
+}
---
+++ week-05/day-03/src/Comparable/ToDo.java
@@ -0,0 +1,9 @@
+package Comparable;
+
+public class ToDo implements Comparable<ToDo>{
+
+    @Override
+    public int compareTo(ToDo o) {
+        return 0;
+    }
+}
