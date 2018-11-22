123dfc542bf20461b4e648294caaee703ccccbb6
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-22 15:41:29 +0100

    isNotNull case added to the test

--- week-05/day-04/test/MyClassTest.java
+++ week-05/day-04/test/MyClassTest.java
@@ -5,49 +5,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyClassTest {
 
-    MyClass myObject;
+  MyClass myObject;
 
-    @BeforeEach
-    void setup() {
-        //myObject = new MyClass();
+  @BeforeEach
+  void setup() {
+    myObject = new MyClass();
+  }
 
-    }
+  @Test
+  void addNumbersToTheList() {
+    myObject.getIntegers().add(5);
+    myObject.getIntegers().add(4);
+    myObject.getIntegers().add(2);
+    myObject.getIntegers().add(1);
+    assertFalse(myObject.getIntegers().isEmpty());
 
-    @Test
-    void addNumbersToTheList() {
-        myObject = new MyClass();
-        myObject.getIntegers().add(5);
-        myObject.getIntegers().add(4);
-        myObject.getIntegers().add(2);
-        myObject.getIntegers().add(1);
-        assertFalse(myObject.getIntegers().isEmpty());
+  }
 
-    }
+  @Test
+  void sumNumbers() {
+    myObject.getIntegers().add(5);
+    myObject.getIntegers().add(4);
+    myObject.getIntegers().add(2);
+    myObject.getIntegers().add(1);
+    assertEquals(12, myObject.sum(myObject.getIntegers()));
 
-    @Test
-    void sumNumbers() {
-        myObject.getIntegers().add(5);
-        myObject.getIntegers().add(4);
-        myObject.getIntegers().add(2);
-        myObject.getIntegers().add(1);
-        assertEquals(12, myObject.sum(myObject.getIntegers()));
+  }
 
-    }
+  @Test
+  void emptyList() {
+    assertEquals(0, myObject.sum(myObject.getIntegers()));
 
-    @Test
-    void emptyList(){
-        assertEquals(0,myObject.sum(myObject.getIntegers()));
+  }
 
-    }
+  @Test
+  void oneElement() {
+    myObject.getIntegers().add(5);
+    assertEquals(5, myObject.sum(myObject.getIntegers()));
+  }
 
-    @Test
-    void oneElement(){
-        myObject.getIntegers().add(5);
-        assertEquals(5, myObject.sum(myObject.getIntegers()));
-    }
-
-    @Test
-    void isNull(){
-        assertNull(myObject);
-    }
+  @Test
+  void isNotNull() {
+    assertNotNull(myObject);
+  }
 }
