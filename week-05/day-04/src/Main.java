661e4df012be46dedd8d50a9f96174eb7c4b4e9b
Enikő Tóthová <toeraab.avf@gmail.com>
2018-11-25 09:51:53 +0100

    runs cows&bulls number guessing game

--- week-05/day-04/src/Main.java
+++ week-05/day-04/src/Main.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.Test;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     CowsAndBulls myGame = new CowsAndBulls();
 
     myGame.playGame();
